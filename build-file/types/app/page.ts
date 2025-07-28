81007af43bd19bc636836870ba5edb3f01c7d746
Muntasir Udoy <muntasirudoy4@gmail.com>
2025-07-29 00:02:12 +0600

    done

--- build-file/types/app/page.ts
+++ build-file/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\10ms\nextjs-15-starter-tailwind\src\app\page.tsx
+// File: D:\10ms\dev-file\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
